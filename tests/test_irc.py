e33e9415ddd1642ad57f1bec841c895c3e20bdff
Mark McGuire <mark.b.mcg@gmail.com>
2013-12-22 20:52:16 -0600

    Add test skelaton for irc

---
+++ tests/test_irc.py
@@ -0,0 +1,48 @@
+import unittest
+
+
+class IrcTest(unittest.TestCase):
+    def test_add(self):
+        pass
+
+    def test_add_twice(self):
+        pass
+
+    def test_add_captain(self):
+        pass
+
+    def test_remove(self):
+        pass
+
+    def test_remove_non_existent(self):
+        pass
+
+    def test_pick(self):
+        pass
+
+    def test_pick_non_existent(self):
+        pass
+
+    def test_pick_already_picked_player(self):
+        pass
+
+    def test_pick_by_non_captain(self):
+        pass
+
+    def test_pick_by_wrong_captain(self):
+        pass
+
+    def test_pick_already_picked_class(self):
+        pass
+
+    def test_unstaged_nick_changes(self):
+        pass
+
+    def test_staged_nick_changes(self):
+        pass
+
+    def test_captains_nick_changes(self):
+        pass
+
+    def test_picked_nick_changes(self):
+        pass
